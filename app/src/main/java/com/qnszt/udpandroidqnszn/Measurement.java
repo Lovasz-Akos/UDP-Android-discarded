635a5b4eb5fc68ccd33e694a6275e5b6a577b8b7
Dombi Tibor <dombidav@gmail.com>
2020-07-09 18:43:42 +0200

    Added Measurement class

--- app/src/main/java/com/qnszt/udpandroidqnszn/Measurement.java
+++ app/src/main/java/com/qnszt/udpandroidqnszn/Measurement.java
@@ -1,4 +1,39 @@
 package com.qnszt.udpandroidqnszn;
 
 public class Measurement {
+    static String Name;
+    static Integer Duration;
+    static Integer Delay;
+
+    public static String getName() {
+        return Name;
+    }
+
+    public static void setName(String name) {
+        Name = name;
+    }
+
+    public static Integer getDuration() {
+        return Duration;
+    }
+
+    public static void setDuration(String duration) {
+        setDuration(Integer.parseInt(duration));
+    }
+
+    public static void setDuration(Integer duration) {
+        Duration = duration;
+    }
+
+    public static Integer getDelay() {
+        return Delay;
+    }
+
+    public static void setDelay(String delay) {
+        setDelay(Integer.parseInt(delay));
+    }
+
+    public static void setDelay(Integer delay) {
+        Delay = delay;
+    }
 }
