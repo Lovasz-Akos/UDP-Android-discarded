5bd3712c7f88ad4a8f31b4928a3df93b6f194510
Lovász Ákos <akos4x44@gmail.com>
2020-07-09 16:27:34 +0200

    added new message sender method
    didnt't work

--- app/src/main/java/com/qnszt/udpandroidqnszn/MainActivity.java
+++ app/src/main/java/com/qnszt/udpandroidqnszn/MainActivity.java
@@ -8,12 +8,26 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Handler;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
+
+            private TextView mTextViewReplyFromServer;
+            private EditText mEditTextSendMessage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +40,11 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "szia tibi evileg sent", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                    sendMessage("Szia tibi :)");
+
             }
         });
     }
@@ -53,4 +70,52 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void sendMessage(final String message) {
+
+        final Handler handler = new Handler();
+        Thread thread = new Thread(new Runnable() {
+
+
+            String stringData;
+
+            @Override
+            public void run() {
+
+                DatagramSocket ds = null;
+                try {
+                    ds = new DatagramSocket();
+                    // IP Address below is the IP address of that Device where server socket is opened.
+                    InetAddress serverAddr = InetAddress.getByName("192.168.43.255");
+                    DatagramPacket dp;
+                    dp = new DatagramPacket(message.getBytes(), message.length(), serverAddr, 1883);
+                    ds.send(dp);
+
+                    byte[] lMsg = new byte[1000];
+                    dp = new DatagramPacket(lMsg, lMsg.length);
+                    ds.receive(dp);
+                    stringData = new String(lMsg, 0, dp.getLength());
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (ds != null) {
+                        ds.close();
+                    }
+                }
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        String s = mTextViewReplyFromServer.getText().toString();
+                        if (stringData.trim().length() != 0)
+                            mTextViewReplyFromServer.setText(s + "\nFrom Server : " + stringData);
+
+                    }
+                });
+            }
+        });
+    }
+
 }
